dc58ce5f48c369a31303b7ef0c4ce115e277ddd6
Kishore <151602177+Kishore-1236@users.noreply.github.com>
2026-02-16 22:45:12 +0530

    Delete Excel_project directory

--- Excel_project/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, upload_excel, get_sheets, get_columns
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('upload/', upload_excel, name='upload_excel'),
-    path('get-sheets/', get_sheets, name='get_sheets'),
-    path('get-columns/', get_columns, name='get_columns'),
-]
